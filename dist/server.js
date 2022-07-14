6de351d7764be52378a1d514a3acea23b3b88995
Hima Vasanth <himavasanth@Himas-MacBook-Pro.local>
2022-07-15 02:12:28 +0530

    removed .gitignore files

--- dist/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-var PORT = 5000 || '';
-app_1.app.listen(PORT, function () { return console.log('listening on port'); });
